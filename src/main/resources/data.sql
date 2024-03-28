be2c85d833cef01beda362f431d6653b8d1bc418
Jakub Wielgocki <jakubwielgocki02@gmail.com>
2024-03-28 11:43:50 +0100

    Added spring data jps, schema and sample data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,100 @@
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Ariana Grande', 'Tik Tok');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Shawn Mendes', 'Bad Guy');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Taylor Swift', 'Shape of You');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Ed Sheeran', 'Love Story');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Billie Eilish', 'Thank U, Next');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Adele', 'Believer');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Post Malone', 'Senorita');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Drake', 'Havana');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Justin Bieber', 'Uptown Funk');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Dua Lipa', 'All About That Bass');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Bruno Mars', 'Rolling in the Deep');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Rihanna', 'Cant Stop the Feeling');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Imagine Dragons', 'Blank Space');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'The Weeknd', 'Shake It Off');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Katy Perry', 'Sorry');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Coldplay', 'Happier');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Beyonce', 'Chandelier');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Maroon 5', 'Royals');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Cardi B', 'Wrecking Ball');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Lizzo', 'Thinking Out Loud');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Sam Smith', 'Hotline Bling');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Taylor Swift', 'Love Yourself');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Ed Sheeran', 'Watermelon Sugar');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Post Malone', 'Dance Monkey');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Ariana Grande', 'Stressed Out');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Shawn Mendes', 'Someone Like You');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Billie Eilish', 'Blinding Lights');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Dua Lipa', 'Cant Feel My Face');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Bruno Mars', 'WAP');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Rihanna', 'Firework');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Imagine Dragons', 'Just the Way You Are');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Katy Perry', 'Grenade');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Beyonce', 'This Love');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Maroon 5', 'Poker Face');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Cardi B', 'Shape of You');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Lizzo', 'Call Me Maybe');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Sam Smith', 'We Found Love');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Taylor Swift', 'Locked Out of Heaven');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Ed Sheeran', 'Love Me Like You Do');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Post Malone', 'Radioactive');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Ariana Grande', 'Titanium');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Shawn Mendes', 'Stay');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Billie Eilish', 'Happy');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Dua Lipa', 'Royals');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Bruno Mars', 'Blank Space');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Rihanna', 'All of Me');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Imagine Dragons', 'Counting Stars');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Katy Perry', 'Take Me to Church');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Beyonce', 'The Hills');
+INSERT INTO song (id, artist, name)
+VALUES (nextval('song_id_seq'), 'Maroon 5', 'Despacito');
