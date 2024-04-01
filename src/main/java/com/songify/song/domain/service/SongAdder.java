2ea34032188cc695b4dabfd77136a5bb65c2d614
Jakub Wielgocki <jakubwielgocki02@gmail.com>
2024-04-01 17:24:01 +0200

    Transactional to rest

--- src/main/java/com/songify/song/domain/service/SongAdder.java
+++ src/main/java/com/songify/song/domain/service/SongAdder.java
@@ -2,11 +2,13 @@ package com.songify.song.domain.service;
 
 import com.songify.song.domain.model.Song;
 import com.songify.song.domain.repository.SongRepository;
+import jakarta.transaction.Transactional;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 @Log4j2
 @Service
+@Transactional
 public class SongAdder {
 
     private final SongRepository songRepository;
