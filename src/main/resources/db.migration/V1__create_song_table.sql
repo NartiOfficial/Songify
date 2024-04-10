cf5cba4c0abf3b1330ef333364100a220c8689f4
NartiOfficial <jakubwielgocki02@gmail.com>
2024-04-10 15:13:14 +0200

    Added flyway

---
+++ src/main/resources/db.migration/V1__create_song_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE song
+(
+    id           BIGSERIAL PRIMARY KEY,
+    name         VARCHAR(255) NOT NULL,
+    artist       VARCHAR(255) NOT NULL,
+    release_date TIMESTAMP WITHOUT TIME ZONE,
+    duration     BIGINT,
+    language     VARCHAR(255)
+);
