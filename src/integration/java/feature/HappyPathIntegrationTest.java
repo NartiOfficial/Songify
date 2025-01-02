b731257be803419eb86b1c440e528596c7bb8156
Jakub Wielgocki <jakubwielgocki02@gmail.com>
2025-01-02 21:59:04 +0100

    test: integration tests packages init

---
+++ src/integration/java/feature/HappyPathIntegrationTest.java
@@ -0,0 +1,15 @@
+package feature;
+
+import org.junit.jupiter.api.Test;
+
+class HappyPathIntegrationTest {
+
+    @Test
+    public void f(){}
+
+    @Test
+    public void fa(){}
+
+    @Test
+    public void fs(){}
+}
