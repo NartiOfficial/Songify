d21d81b802debe8e3a4696895a6d26eba84d904f
Jakub Wielgocki <jakubwielgocki02@gmail.com>
2025-01-15 23:49:45 +0100

    feat: Added simple security config

---
+++ src/main/java/com/songify/infrastructure/security/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.songify.infrastructure.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var manager = new InMemoryUserDetailsManager();
+        var user1 = User.withUsername("User")
+                .password("12345")
+                .roles("USER")
+                .build();
+        var user2 = User.withUsername("User2")
+                .password("12345")
+                .roles("USER", "ADMIN")
+                .build();
+        manager.createUser(user1);
+        manager.createUser(user2);
+        return manager;
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
