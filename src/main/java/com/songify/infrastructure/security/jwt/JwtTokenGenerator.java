19502be9439120349a82445c5c2b84716598f047
Jakub Wielgocki <jakubwielgocki02@gmail.com>
2025-01-16 20:52:20 +0100

    feat: Used symmetric key to sign token, and secret from env variable

--- src/main/java/com/songify/infrastructure/security/jwt/JwtTokenGenerator.java
+++ src/main/java/com/songify/infrastructure/security/jwt/JwtTokenGenerator.java
@@ -1,6 +1,7 @@
 package com.songify.infrastructure.security.jwt;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.songify.infrastructure.security.SecurityUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,6 +19,8 @@ import java.time.ZoneOffset;
 @RequiredArgsConstructor
 class JwtTokenGenerator {
 
+    private static final String ROLES_CLAIM_NAME = "roles";
+
     private final AuthenticationManager authenticationManager;
     private final Clock clock;
     private final JwtConfigurationProperties properties;
@@ -28,12 +31,13 @@ class JwtTokenGenerator {
         SecurityUser securityUser = (SecurityUser) authentication.getPrincipal();
         Instant issuedAt = LocalDateTime.now(clock).toInstant(ZoneOffset.UTC);
         Instant expiresAt = issuedAt.plus(Duration.ofMinutes(properties.expirationMinutes()));
+        Algorithm algorithm = Algorithm.HMAC256(properties.secret());
         return JWT.create()
                 .withSubject(securityUser.getUsername())
                 .withIssuedAt(issuedAt)
                 .withExpiresAt(expiresAt)
                 .withIssuer(properties.issuer())
-                .withClaim("roles", securityUser.getAuthoritiesAsString())
-                .sign(null);
+                .withClaim(ROLES_CLAIM_NAME, securityUser.getAuthoritiesAsString())
+                .sign(algorithm);
     }
 }
