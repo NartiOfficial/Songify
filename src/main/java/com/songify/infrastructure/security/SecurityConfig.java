c911ab6c9612f360848e0abd8aa4fb2a663b9d42
Jakub Wielgocki <jakubwielgocki02@gmail.com>
2025-01-16 16:11:30 +0100

    feat: Exposed for all GET endpoints

--- src/main/java/com/songify/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/songify/infrastructure/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.songify.infrastructure.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -27,6 +28,10 @@ class SecurityConfig {
                 .requestMatchers("/swagger-resources/**").permitAll()
                 .requestMatchers("/v3/api-docs/**").permitAll()
                 .requestMatchers("/auth/register/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/songs/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/artists/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/albums/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/genres/**").permitAll()
                 .anyRequest().authenticated());
         return http.build();
     }
