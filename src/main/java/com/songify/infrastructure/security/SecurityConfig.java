349039a8f022e09375d8a9435585adef4205e157
Jakub Wielgocki <jakubwielgocki02@gmail.com>
2025-01-16 18:42:10 +0100

    feat: Delegated auth to manager

--- src/main/java/com/songify/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/songify/infrastructure/security/SecurityConfig.java
@@ -3,7 +3,9 @@ package com.songify.infrastructure.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
@@ -22,6 +24,11 @@ class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 
     @Bean
--- src/main/java/com/songify/infrastructure/security/jwt/JwtTokenGenerator.java
+++ src/main/java/com/songify/infrastructure/security/jwt/JwtTokenGenerator.java
@@ -1,10 +1,20 @@
 package com.songify.infrastructure.security.jwt;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 class JwtTokenGenerator {
+
+    private final AuthenticationManager authenticationManager;
+
     public String authenticateAndGenerateToken(String username, String password) {
-        return "token123";
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+        Authentication authentication = authenticationManager.authenticate(authenticationToken);
+        return "123";
     }
 }
