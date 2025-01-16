ccfd7f3d45c0915d03dfbaeaa1d3ea35bc2b51b9
Jakub Wielgocki <jakubwielgocki02@gmail.com>
2025-01-16 15:18:23 +0100

    feat: All request authenticated

--- src/main/java/com/songify/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/songify/infrastructure/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.songify.infrastructure.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,6 +20,10 @@ class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable);
+        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+        http.authorizeHttpRequests(authorizeRequests ->
+                authorizeRequests.anyRequest().authenticated());
         return http.build();
     }
 }
