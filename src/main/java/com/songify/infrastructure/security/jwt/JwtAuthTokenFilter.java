5bc4d870e1dc5b893e9f468dfd8359e45cf751d1
Jakub Wielgocki <jakubwielgocki02@gmail.com>
2025-03-03 11:46:27 +0100

    feat: Filter for auth token verifying, symmetric key and asymmetric key approaches, and also swagger config for adding Authorization Bearer Header

--- src/main/java/com/songify/infrastructure/security/jwt/JwtAuthTokenFilter.java
+++ src/main/java/com/songify/infrastructure/security/jwt/JwtAuthTokenFilter.java
@@ -1,19 +1,53 @@
 package com.songify.infrastructure.security.jwt;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
+import java.security.KeyPair;
+import java.security.interfaces.RSAPublicKey;
+import java.util.List;
+
 @Component
 @Log4j2
+@RequiredArgsConstructor
 public class JwtAuthTokenFilter extends OncePerRequestFilter {
+
+    private final KeyPair keyPair;
+    private final JwtConfigurationProperties properties;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorization = request.getHeader("Authorization");
+        if (authorization == null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        JWTVerifier jwtVerifier = JWT.require(Algorithm.RSA256((RSAPublicKey) keyPair.getPublic(), null))
+                .build();
+//        String secret = properties.secret(); SYMMETRIC KEY APPROACH
+//        JWTVerifier jwtVerifier = JWT.require(Algorithm.HMAC256(secret)).build();
+        String token = authorization.substring(7);
+        DecodedJWT decodedToken = jwtVerifier.verify(token);
+        String login = decodedToken.getSubject();
+        List<SimpleGrantedAuthority> roles = decodedToken.getClaim("roles")
+                .asList(String.class)
+                .stream()
+                .map(SimpleGrantedAuthority::new)
+                .toList();
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(login, null, roles);
+        SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
         filterChain.doFilter(request, response);
     }
 }
